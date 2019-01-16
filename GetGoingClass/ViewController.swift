f6a5d35693cea72b03e763b6b7f47999f1c6950c
frau <allab@ca.ibm.com>
2019-01-16 18:04:13 -0400

    Initial Commit

---
+++ GetGoingClass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetGoingClass
+//
+//  Created by Alla Bondarenko on 2019-01-16.
+//  Copyright © 2019 SMU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
